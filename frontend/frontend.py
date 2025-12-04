6ccc8e2d16b27db12642703886869c3815cdf0a1
Argh Jain <arghjain04@gmail.com>
2025-12-05 00:16:21 +0530

    changes in reg

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -16,8 +16,7 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
-BACKEND_URL = "https://manufacturer-backend.onrender.com"
-
+BACKEND_URL = "http://localhost:5000"
 
 st.set_page_config(
     page_title="Manufacturer Trust Evaluation",
